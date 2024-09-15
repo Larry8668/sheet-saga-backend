e8baa797c01c210573fcc41fa9cc0155e5837ec2
Larry8668 <114809719+Larry8668@users.noreply.github.com>
2024-09-15 15:12:49 +0530

    log works so far

--- src/routes/syncRoutes.js
+++ src/routes/syncRoutes.js
@@ -1,10 +1,12 @@
 const express = require("express");
 const { handleSync } = require("../controllers/syncController");
 const { updateSheet } = require("../controllers/updateSheetController");
+const { logChange } = require("../controllers/logController");
 
 const router = express.Router();
 
 router.post("/sync", handleSync);
 router.post("/update-cell", updateSheet)
+router.post("/log-change", logChange);
 
 module.exports = router;
